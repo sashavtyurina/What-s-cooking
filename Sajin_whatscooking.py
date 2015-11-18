54934507e33380b4fafce22aa7a9cbdb0ddbe363
Alexandra Vtyurina <sasha.vtyurina@gmail.com>
2015-11-17 23:00:58 -0500

    removed list comprehension

--- Sajin_whatscooking.py
+++ Sajin_whatscooking.py
@@ -45,11 +45,29 @@ def cross_validate(text_clf, X, Y, folds=10):
     for train_indices, test_indices in skf:  # these are indices of train and test sets
         # number of passes should be equal to the number of folds
         error = 0
-        Xtrain = [X[i] for i in train_indices]
-        Ytrain = [Y[i] for i in train_indices]
 
-        Xtest = [X[i] for i in test_indices]
-        Ytest = [Y[i] for i in test_indices]
+        Xtrain = []
+        Ytrain = []
+        Xtest = []
+        Ytest = []
+
+        for i in train_indices:
+            Xtrain.append(X[i])
+
+        for i in train_indices:
+            Ytrain.append(Y[i])
+
+        for i in test_indices:
+            Xtest.append(X[i])
+
+        for i in test_indices:
+            Ytest.append(Y[i])
+
+        # Xtrain = [X[i] for i in train_indices]
+        # Ytrain = [Y[i] for i in train_indices]
+        #
+        # Xtest = [X[i] for i in test_indices]
+        # Ytest = [Y[i] for i in test_indices]
 
         trained_model = text_clf.fit(Xtrain, Ytrain)
         predicted = text_clf.predict(Xtest)
@@ -57,11 +75,14 @@ def cross_validate(text_clf, X, Y, folds=10):
         for i in range(0, len(predicted)):
             error += predicted[i] != Ytest[i]
         error /= (len(predicted) + 1)
-        print(error)
+        # print(error)
+        print error
         averaged_error += error
 
     averaged_error /= folds
-    print(averaged_error)
+
+    # print(averaged_error)
+    print averaged_error
 
     return averaged_error
 
@@ -72,11 +93,11 @@ data_train = pd.read_json('train.json')
 N_train = np.shape(data_train)[0]
 data_test = pd.read_json('test.json')
 N_test = np.shape(data_test)[0]
-print('%d, %d ' % (N_train, N_test))
+# print('%d, %d ' % (N_train, N_test))
 
 text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(1, 5))),
                      #('tfidf', TfidfTransformer()),
-                     ('clf',SGDClassifier(loss='modified_huber', penalty='l2', alpha=1e-4, n_iter=10, random_state=7))])
+                     ('clf', SGDClassifier(loss='modified_huber', penalty='l2', alpha=1e-4, n_iter=10, random_state=7))])
 
 
 
@@ -87,34 +108,35 @@ for i in data_train.ingredients:
 result = text_clf.fit(train_ingredients, data_train.cuisine)
 
 cross_validate(text_clf, train_ingredients, data_train.cuisine)
-input()
+
 
 predict_train = text_clf.predict(train_ingredients)
 
 
 
-#Converting Test Json Ingredient List to String for CountVectorizer compatibility
+# Converting Test Json Ingredient List to String for CountVectorizer compatibility
 test_ingredients = []
 for i in data_test.ingredients:
     test_ingredients.append(str(i))
 predict_test = text_clf.predict(test_ingredients)
 
-#Print Training Error
+# Print Training Error
 err = 0
 for i in range(0, N_train):
     if predict_train[i] != data_train.cuisine[i]:
         err += 1
-print('Training Error : %f' % (float(err)/N_train))
-print('%d, %f' % (N_train, err))
+# print('Training Error : %f' % (float(err)/N_train))
+# print('%d, %f' % (N_train, err))
 
-#Create submission file
+# Create submission file
 with open('submission.csv', 'w+') as out_file:
     out_file.write('id,cuisine\n')
     for i in range(0, N_test):
         out_file.write(str(data_test.id[i]) + ',' + predict_test[i] + '\n')
 
 # Cross-validation bit
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    train_ingredients, data_train.cuisine, test_size=0.4, random_state=0)
+# X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+#     train_ingredients, data_train.cuisine, test_size=0.4, random_state=0)
+
 
 
